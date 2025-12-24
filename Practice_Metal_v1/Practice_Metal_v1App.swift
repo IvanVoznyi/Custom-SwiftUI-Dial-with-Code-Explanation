18494c5cabd5adc0fc0fc86e6eb09228ee2423e3
Ivan Voznyi <ivan.voznyi92@gmail.com>
2025-12-24 13:03:58 +0200

    Initial Commit

---
+++ Practice_Metal_v1/Practice_Metal_v1App.swift
@@ -0,0 +1,17 @@
+//
+//  Practice_Metal_v1App.swift
+//  Practice_Metal_v1
+//
+//  Created by Ivan Voznyi on 12/24/25.
+//
+
+import SwiftUI
+
+@main
+struct Practice_Metal_v1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
